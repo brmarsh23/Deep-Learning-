d150e79f381389e874ba76de9f1da5379d035f55
brmarsh23 <brmarsh76@gmail.com>
2023-03-06 11:59:48 +0300

    added dataset construction into the basics file.

--- Keras Tensorflow Basics.py
+++ Keras Tensorflow Basics.py
@@ -1,6 +1,6 @@
 # In this section we will import the layers needed to execute the code
 
-
+import numpy as np
 from random import randint
 from sklearn.utils import shuffle
 from sklearn.preprocessing import MinMaxScaler
@@ -38,4 +38,18 @@ for i in range(1000):
     random_older = randint(65,100)
     train_samples.append(random_older)
     train_labels.append(1)
+
+# convert the train labels and samples lists into numpy arrays
+# then shuffle them to eliminate ordering
+# finally, we will scale the training samples down to between 0 to 1
+train_samples = np.array(train_samples)
+train_labels = np.array(train_labels)
+train_samples, train_labels = shuffle(train_samples, train_labels)
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1,1))
+
+# let's print out the scaled train samples
+for i in scaled_train_samples:
+    print(i)
 

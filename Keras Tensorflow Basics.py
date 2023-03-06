2899404c480404e4e1b7167d673b27fdb0b66999
brmarsh23 <brmarsh76@gmail.com>
2023-03-06 11:54:04 +0300

    added dataset construction into the basics file.

--- Keras Tensorflow Basics.py
+++ Keras Tensorflow Basics.py
@@ -1,13 +1,6 @@
 # In this section we will import the layers needed to execute the code
 
-import tensorflow
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Activation, Dense, BatchNormalization, Conv2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.metrics import categorical_crossentropy
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import numpy as np
+
 from random import randint
 from sklearn.utils import shuffle
 from sklearn.preprocessing import MinMaxScaler

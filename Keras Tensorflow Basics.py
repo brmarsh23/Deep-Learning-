4e196238fcadff1faed1f5f74eb5d0126888059d
brmarsh23 <brmarsh76@gmail.com>
2023-03-09 11:31:12 +0300

    added model construction into the basics file

--- Keras Tensorflow Basics.py
+++ Keras Tensorflow Basics.py
@@ -1,9 +1,23 @@
-# In this section we will import the layers needed to execute the code
-
+########################################################################
+# In this section we will import the libraries needed to execute the code
+########################################################################
+# These are the libraries needed to create and process the training dataset and labels
 import numpy as np
 from random import randint
 from sklearn.utils import shuffle
 from sklearn.preprocessing import MinMaxScaler
+
+# These are the libraries needed to create the deep neural network model
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+
+########################################################################
+# In this section we will create a dataset along with corresponding labels for training
+########################################################################
 
 # First, lets grab some data. We need samples and their corresponding labels.
 
@@ -51,6 +65,30 @@ scaler = MinMaxScaler(feature_range=(0,1))
 scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1,1))
 
 # let's print out the scaled train samples
-for i in scaled_train_samples:
-    print(i)
+#for i in scaled_train_samples:
+    #print(i)
 
+########################################################################
+# Now, we will create the sequential model using Tensorflow
+########################################################################
+
+# First, let's check to see if Tensorflow recognizes that we have a GPU
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(physical_devices))
+
+
+# Now, let's build the model.  Using the Sequential object, tf.keras will build a sequential stack of layers.
+# Sequential Object accepts a list, and each element of the list should be a layer
+
+# notice that the input layer is not strictly specified beyond the input_shape argument
+# in this example, only the 3 dense layers are defined within the list fed to the Sequential Object
+
+model = Sequential([
+    Dense(units=16, input_shape=(1,), activation='relu'),
+    Dense(units=32, activation='relu'),
+    Dense(units=2, activation='softmax')
+])
+
+# Let's print the model out to see what it looks like
+
+print(model.summary())
